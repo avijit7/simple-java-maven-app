59eb8b5697e0fc3c45ba677e98999383f697ce40
avijit7 <41124050+avijit7@users.noreply.github.com>
2019-07-12 10:38:33 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World 07/12/2019 10:33am";
+    private final String message = "Hello World 07/12/2019 10:38am";
 
     public App() {}
 
